1cb1f51bd8b4690a2077f5ee36f55f413b87a8da
Mikey Shean <mdshean2@gmail.com>
2015-08-04 12:16:22 -0400

    Delete Bishop#move_dirs.

--- bishop.rb
+++ bishop.rb
@@ -8,19 +8,5 @@ class Bishop < SlidingPiece
     @color = color
   end
 
-  def move_dirs
-    valid_moves = []
-    DIRECTIONS.each do |coord|
-      x,y = coord
-      row,col = pos
-      new_pos = [row + x, col + y]
-      until !board.on_board?(new_pos)
-        valid_moves << new_pos #if not_obstructed
-        row += x
-        col += y
-        new_pos = [row + x, col + y]
-      end
-    end
-    valid_moves
-  end
+
 end
