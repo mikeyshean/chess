6922a36604e7555b550d7d908a55f652192adc97
Mikey Shean <mdshean2@gmail.com>
2015-08-04 12:15:37 -0400

    Update SlidingPiece#moves.

--- rook.rb
+++ rook.rb
@@ -1,7 +1,10 @@
 require_relative "sliding_piece"
 class Rook < SlidingPiece
-
-  def initialize(pos, board)
-    super
+  DIRECTIONS = [[-1,0], [1,0], [0, 1], [0, -1]]
+  attr_reader :color
+  def initialize(pos, board, color)
+    super(pos, board)
+    @color = color
   end
+
 end
