ef52c3dbbe88e0a1a7515b1ecac67fd947b63d0f
Mikey Shean <mdshean2@gmail.com>
2015-08-04 12:42:30 -0400

    Add SlidingPiece#obstructed?

--- sliding_piece.rb
+++ sliding_piece.rb
@@ -7,18 +7,23 @@ class SlidingPiece < Piece
 
   def moves
     valid_moves = []
-    self.move_dirs.each do |coord|
+    move_dirs.each do |coord|
       x,y = coord
       row,col = pos
       new_pos = [row + x, col + y]
       until !board.on_board?(new_pos)
-        valid_moves << new_pos #if not_obstructed
+        break if obstructed?(new_pos)
+        valid_moves << new_pos
         row += x
         col += y
         new_pos = [row + x, col + y]
       end
     end
     valid_moves
+  end
+
+  def obstructed?(new_pos)
+    !board[new_pos].nil? && board[new_pos].color == board[pos].color
   end
 
 end
