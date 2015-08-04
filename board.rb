ed68dc40002865aa25c752e84dd92be6ac7618e6
Mikey Shean <mdshean2@gmail.com>
2015-08-04 11:57:28 -0400

    Add syntactic sugar to Board class

--- board.rb
+++ board.rb
@@ -8,4 +8,14 @@ class Board
     @grid = Array.new(8) { Array.new(8) }
   end
 
+  def [](pos)
+    row, col = pos
+    @grid[row][col]
+  end
+
+  def []=(pos, value)
+    row, col = pos
+    @grid[row][col] = value
+  end
+
 end
