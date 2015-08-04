9442b437d10f6d02f3161657b92ae723f8aaaa7f
Mikey Shean <mdshean2@gmail.com>
2015-08-04 13:53:15 -0400

    Add King#move_dirs.

--- king.rb
+++ king.rb
@@ -1,6 +1,20 @@
 require_relative 'stepping_piece'
 class King
+  DIRECTIONS = [
+    [-1,  0],
+    [ 1,  0],
+    [ 0,  1],
+    [ 0, -1],
+    [-1,  1],
+    [ 1,  1],
+    [-1, -1],
+    [ 1, -1]
+  ]
   def initialize(pos, board, color)
     super
+  end
+
+  def move_dirs
+    DIRECTIONS
   end
 end
