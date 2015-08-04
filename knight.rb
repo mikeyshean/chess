5ae0ad12f71f7f286d962d7dc2ea2556ab0432fd
Mikey Shean <mdshean2@gmail.com>
2015-08-04 13:47:41 -0400

    Add Knight#move_dirs.

--- knight.rb
+++ knight.rb
@@ -1,10 +1,21 @@
 require_relative 'stepping_piece'
 class Knight < SteppingPiece
-
+  DIRECTIONS = [
+   [-2, -1],
+   [-2,  1],
+   [-1, -2],
+   [-1,  2],
+   [ 1, -2],
+   [ 1,  2],
+   [ 2, -1],
+   [ 2,  1]
+  ]
   def initialize(pos, board, color)
     super(pos, board)
     @color = color
   end
 
-  
+  def move_dirs
+    DIRECTIONS
+  end
 end
