d139138ee6401904186be47f29f5ab691924e227
Mikey Shean <mdshean2@gmail.com>
2015-08-05 11:59:02 -0400

    Add Game#switch_player & Game#over? helpers.

--- game.rb
+++ game.rb
@@ -1,8 +1,21 @@
 require_relative 'board'
 class Game
-  attr_reader :board
+  attr_reader :board, :players, :current_player
 
   def initialize
-     board = Board.new
+     @board = Board.new
+     @players = [:white, :black]
+     @current_player = players.first
   end
+
+
+
+  def over?
+    board.checkmate?(:white) || board.checkmate?(:black)
+  end
+  
+  def switch_player
+    players.rotate!
+  end
+
 end
